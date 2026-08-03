8d0bb6e6c448e045ce124debc962924df3a5864a
Laksh Sharma <laksh1441942@gmail.com>
2026-08-03 23:41:17 +0530

    Time: 47 ms (77.12%) | Memory: 24.3 MB (92.04%) - LeetSync

---
+++ Graph_Questions/1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        int n = heights.size();
+        int m = heights[0].size();
+        priority_queue<pair<int,pair<int,int>>,
+        vector<pair<int,pair<int,int>>>,
+        greater<pair<int,pair<int,int>>>>pq;
+        vector<vector<int>> diff(n, vector<int>(m, 1e9));
+        diff[0][0] = 0;
+        pq.push({0,{0,0}});
+        int dr[4] = {-1, 1, 0, 0};
+        int dc[4] = {0, 0, 1, -1};
+        while(!pq.empty()){
+            auto it = pq.top();
+            pq.pop();
+            int dif = it.first;
+            int r = it.second.first;
+            int c = it.second.second;
+            if (r == n-1 && c == m-1 ) return dif;
+            for(int i=0; i<4; i++){
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+                if(nr>=0 && nr<n && nc>=0 && nc<m ){
+                    int d = max(abs(heights[r][c] - heights[nr][nc]),dif);
+                    if( d < diff[nr][nc]){
+                        diff[nr][nc] = d;
+                        pq.push({d, {nr, nc}});
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+};
