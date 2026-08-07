b3a4422e3c9ee59eb7894d521371c85b41289b9c
Laksh Sharma <laksh1441942@gmail.com>
2026-08-08 00:21:00 +0530

    Time: 16 ms (21.54%) | Memory: 37.3 MB (45.59%) - LeetSync

---
+++ Graph_Questions/2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int countPaths(int n, vector<vector<int>>& roads) {
+        vector<pair<int,int>> adj[n];
+        for(auto it : roads){
+            adj[it[0]].push_back({it[1],it[2]});
+            adj[it[1]].push_back({it[0], it[2]});
+        }
+        priority_queue<pair<long long,int>,vector<pair<long long,int>>,greater<pair<long long,int>>>pq;
+        vector<long long>dist(n, LLONG_MAX);
+        vector<int>ways(n, 0);
+        int mod = (int)(1e9 + 7);
+        dist[0] = 0;
+        ways[0] = 1;
+        pq.push({0,0});
+        while(!pq.empty()){
+            long long dis = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
+            for(auto it : adj[node]){
+                int adjnode = it.first;
+                int adjdis = it.second;
+                if(dis > dist[node]) continue;
+                if( dis + adjdis < dist[adjnode]){
+                    pq.push({dis + adjdis, adjnode});
+                    dist[adjnode] = dis + adjdis;
+                    ways[adjnode] = ways[node];
+                }
+                else if (dis + adjdis == dist[adjnode]){
+                    ways[adjnode] =( ways[adjnode] + ways[node])%mod;
+                }
+            }
+
+        }
+        return ways[n-1]%mod;
+
+    }
+};
