2d1abc20887a109cfda7bf508fb05bb8980137df
Laksh Sharma <laksh1441942@gmail.com>
2026-08-17 00:52:23 +0530

    Time: 11 ms (81.34%) | Memory: 57 MB (83.18%) - LeetSync

--- Linked_List_questions/148-sort-list/sort-list.cpp
+++ Linked_List_questions/148-sort-list/sort-list.cpp
@@ -1,28 +1,62 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
+    
+    ListNode* merge(ListNode* left, ListNode* right) {
+        
+        ListNode dummy(0);
+        ListNode* curr = &dummy;
+        
+        while (left && right) {
+            
+            if (left->val <= right->val) {
+                curr->next = left;
+                left = left->next;
+            }
+            else {
+                curr->next = right;
+                right = right->next;
+            }
+            
+            curr = curr->next;
+        }
+        
+        if (left)
+            curr->next = left;
+        
+        if (right)
+            curr->next = right;
+        
+        return dummy.next;
+    }
+    
+    ListNode* mergesort(ListNode* head) {
+        
+        // 0 or 1 node is already sorted
+        if (!head || !head->next)
+            return head;
+        
+        // Find middle
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        // Split into two lists
+        ListNode* right = slow->next;
+        slow->next = nullptr;
+        
+        // Sort both halves
+        ListNode* left = mergesort(head);
+        right = mergesort(right);
+        
+        // Merge sorted halves
+        return merge(left, right);
+    }
+
 public:
     ListNode* sortList(ListNode* head) {
-        ListNode* temp = head;
-        vector<int>v;
-        while(temp){
-            v.push_back(temp->val);
-            temp = temp->next;
-        }
-        sort(v.begin(), v.end());
-        temp = head;
-        for(int it : v){
-            temp->val = it;
-            temp = temp->next;
-        }
-        return head;
+        return mergesort(head);
     }
 };
